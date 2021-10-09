30d833bf01837d9491c5f57185f7322605c99cbc
ulyssesmtr <ulyssesdmnt@gmail.com>
2021-10-09 12:21:06 -0300

    Updating the database code

--- database.py
+++ database.py
@@ -1,8 +1,69 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import inspect
+from flask import Response
+
+db = SQLAlchemy(None)
 
 
 def setup(app):
+    global db
+    db = SQLAlchemy(app)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:ulysses123@localhost/tibia_wiki_db'
 
 
+class Creature(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    life = db.Column(db.Integer)
+    exp = db.Column(db.Integer)
+
+    def to_json(self):
+        return {"id": self.id, "name": self.name, "life": self.life, "exp": self.exp}
+
+
+def creating_creature(body):
+    creature = Creature(name=body["name"].title(), life=body["life"], exp=body["exp"])
+    all_creature_names = db.session.query(Creature.name).all()  # Get all the creature names from the db
+    all_creature_names = [n[0].lower() for n in all_creature_names]  # Modify the data to a list of names
+    if creature.name.lower() not in all_creature_names:
+        db.session.add(creature)
+        db.session.commit()
+        return creature.to_json()
+    else:
+        return Response(status=409)
+
+
+def showing_creature(id):
+    creature_object = Creature.query.filter_by(id=id).first()
+
+    return creature_object.to_json()
+
+
+def showing_all_creatures():
+    all_creatures_object_list = Creature.query.all()
+    all_creatures_json_list = [creature.to_json() for creature in all_creatures_object_list]
+
+    return all_creatures_json_list
+
+
+def editing_creature(id, body):
+    creature_object = Creature.query.filter_by(id=id).first()
+    creature_object.name = body["name"]
+    creature_object.life = body["life"]
+    creature_object.exp = body["exp"]
+    current_db_sessions = inspect(creature_object).session
+    current_db_sessions.add(creature_object)
+    # db.session.add(creature_object)
+    # db.session.commit()
+    current_db_sessions.commit()
+    return creature_object.to_json()
+
+
+def deleting_creature(id):
+    creature_object = Creature.query.filter_by(id=id).first()
+    current_db_sessions = inspect(creature_object).session
+    current_db_sessions.delete(creature_object)
+    current_db_sessions.commit()
+
+    return creature_object.to_json()
--- tibia_wiki.py
+++ tibia_wiki.py
@@ -1,26 +1,11 @@
 from flask import Flask, Response, request
-from flask_sqlalchemy import SQLAlchemy
-import mysql.connector
 import json
 from flask_cors import CORS
+import database
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:ulysses123@localhost/tibia_wiki_db'
-
-
+database.setup(app)
 CORS(app)
-db = SQLAlchemy(app)
-
-
-class Creature(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    life = db.Column(db.Integer)
-    exp = db.Column(db.Integer)
-
-    def to_json(self):
-        return {"id": self.id, "name": self.name, "life": self.life, "exp": self.exp}
 
 
 @app.route('/')
@@ -32,23 +17,15 @@ def home():
 @app.route('/create_creature', methods=["POST"])
 def create_creature():
     body = request.get_json()
-    creature = Creature(name=body["name"].title(), life=body["life"], exp=body["exp"])
-    all_creature_names = db.session.query(Creature.name).all()  # Get all the creature names from the db
-    all_creature_names = [n[0].lower() for n in all_creature_names]  # Modify the data to a list of names
-    if creature.name.lower() not in all_creature_names:
-        db.session.add(creature)
-        db.session.commit()
+    creature_json = database.creating_creature(body)
 
-        return creature.to_json()
-    else:
-        return Response(status=409)
+    return creature_json
 
 
 # Route to show the information of a specific creature, identified by its id
 @app.route('/creature/<id>', methods=['GET'])
 def show_creature(id):
-    creature_object = Creature.query.filter_by(id=id).first()
-    creature_json = creature_object.to_json()
+    creature_json = database.showing_creature(id)
 
     return json.dumps(creature_json)
 
@@ -56,33 +33,24 @@ def show_creature(id):
 # Route to show all the creatures
 @app.route('/creatures', methods=['GET'])
 def show_all_creatures():
-    all_creatures_object_list = Creature.query.all()
-    all_creatures_json_list = [creature.to_json() for creature in all_creatures_object_list]
+    all_creatures_json_list = database.showing_all_creatures()
 
     return json.dumps(all_creatures_json_list)
 
 
 # Route to edit a specific creature,
 @app.route('/creature/<id>', methods=['PUT'])
-def search_creature(id):
+def edit_creature(id):
     body = request.get_json()
-    creature_object = Creature.query.filter_by(id=id).first()
+    creature_object_json = database.editing_creature(id, body)
 
-    creature_object.name = body["name"]
-    creature_object.life = body["life"]
-    creature_object.exp = body["exp"]
-    db.session.add(creature_object)
-    db.session.commit()
-
-    return creature_object.to_json()
+    return creature_object_json
 
 
 # Route to delete a specific creature
 @app.route('/creature/<id>', methods=['DELETE'])
 def delete_creature(id):
-    creature_object = Creature.query.filter_by(id=id).first()
-    db.session.delete(creature_object)
-    db.session.commit()
+    creature_object_json = database.deleting_creature(id)
 
-    return creature_object.to_json()
+    return creature_object_json
 
